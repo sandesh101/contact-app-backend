d5abf9ce02e9af58ce35f743e60945a41c9b602a
sandesh101 <rimal.sandesh11@gmail.com>
2023-11-20 13:17:24 +0545

    Dotenv setup

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 
-const PORT = 5000;
+dotenv.config({
+    path: './env',
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (() => console.log(`Server is listening at ${PORT}`)));
