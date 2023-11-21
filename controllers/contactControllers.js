730efab9919253eb99674ed411d7c6a66231dd6a
sandesh101 <rimal.sandesh11@gmail.com>
2023-11-21 22:33:18 +0545

    Backend complete

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -2,14 +2,14 @@ import asyncHandler from 'express-async-handler';
 import { contactModel } from '../models/contactModel.js';
 //Get all the contacts
 const getAllContacts = asyncHandler( async (req, res) => {
-    const contact = await contactModel.find();
+    const contact = await contactModel.find({user_id: req.user.id});
     res.status(200).json(contact);
 });
 
 //Get specific contact
 
 const getContact = asyncHandler( async (req, res) => {
-    const contact = await contactModel.find({user_id: req.user.id});
+    const contact = await contactModel.find(req.user.id);
     if(!contact){
         res.status(400);
         throw new Error("Contact not found!!")
@@ -20,8 +20,7 @@ const getContact = asyncHandler( async (req, res) => {
 
 //Create contacts
 const createContact = asyncHandler( async(req,res) => {
-    console.log(`CONTACT: ${req.body}`);
-    const { id ,name, email, phone} = req.body;
+    const { name, email, phone} = req.body;
 
     if(!name || !email || !phone){
         res.status(400);
@@ -29,10 +28,10 @@ const createContact = asyncHandler( async(req,res) => {
     }
 
     const contact = await contactModel.create({
-        id,
         name,
         email,
-        phone
+        phone,
+        user_id: req.user.id
     });
     res.status(201).json({
         message: "Success", 
@@ -46,6 +45,11 @@ const updateContact = asyncHandler( async (req, res) => {
     if(!contact){
         res.status(400);
         throw new Error("Contact not found!!");
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("User don't have permission");
     }
 
     const updatedContact = await contactModel.findByIdAndUpdate(
@@ -64,7 +68,12 @@ const deleteContact = asyncHandler( async (req,res) => {
         throw new Error("Contact not found!!");
     }
 
-    const deletedContact = await contactModel.findByIdAndDelete(req.params.id);
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("User don't have permission");
+    }
+
+    const deletedContact = await contactModel.deleteOne({ _id: req.params.id });
     res.status(200).json({message: `Successfull`})
 });
 
